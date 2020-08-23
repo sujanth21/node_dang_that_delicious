2b0bdb019e53254f7da70da0c9919025e691f875
sujanth21 <sujanth1987@gmail.com>
2020-08-23 20:52:23 +1000

    Store model and routers added

--- controllers/stores.js
+++ controllers/stores.js
@@ -11,3 +11,34 @@ exports.getHomePage = (req, res, next) => {
     });
   }
 };
+
+// @desc    Get a add store page
+// @route   GET /add
+// @access  Private
+exports.addStore = (req, res, next) => {
+  try {
+    res.render("editStore", { title: "Add Store" });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      error,
+    });
+  }
+};
+
+// @desc   Create a new store and stored to a database
+// @route  POST /add
+// @access Private
+exports.createStore = (req, res, next) => {
+  try {
+    res.status(201).json({
+      success: true,
+      data: req.body,
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      error,
+    });
+  }
+};
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-const { getHomePage } = require("../controllers/stores");
+const { getHomePage, addStore, createStore } = require("../controllers/stores");
 
 // Do work here
 router.route("/").get(getHomePage);
+router.route("/add").get(addStore);
+router.route("/add").post(createStore);
 
 module.exports = router;
